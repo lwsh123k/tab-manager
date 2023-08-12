9746630b26c3a4219b52ffdc3127ab1d62429603
lwsh123k <lwsh123k@163.com>
2023-08-12 12:47:50 +0800

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 console.log("加载成功了");
 // 监听来自网页的消息
+//
 chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResponse) {
     console.log(message);
     if (message.url === blocklistedWebsite) return; // don't allow this web page access
